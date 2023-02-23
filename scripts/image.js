95d140f08e79fa464751538c3d601cc1db68098a
Monir Hoshen <getmonirr@gmail.com>
2023-02-23 12:33:27 +0600

    image gallary done

---
+++ scripts/image.js
@@ -0,0 +1,54 @@
+
+const maxItems = 10;
+let prevLoad = 0;
+let nextLoad = 10;
+
+const displayGalleryUI = (title, url, albumId, id) => {
+
+    const galleryContainer = document.getElementById('gallery-container');
+    ('load-more-container');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card w-96 bg-base-100 shadow-xl">
+    <figure><img src="${url}" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        ${title}
+        <div class="badge badge-secondary">NEW</div>
+      </h2>
+      <div class="card-actions justify-end">
+        <div class="badge badge-outline">Album Id: ${albumId}</div> 
+        <div class="badge badge-outline">Photo Id: ${id}</div>
+      </div>
+    </div>
+  </div>
+    `;
+    galleryContainer.appendChild(div);
+}
+
+const getImgData = () => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(res => res.json())
+        .then(data => setImg(data))
+}
+
+const setImg = (data) => {
+    data.slice(prevLoad, nextLoad).map(img => {
+        const { title, id, url, albumId } = img;
+        displayGalleryUI(title, url, albumId, id);
+    })
+}
+
+const handleLoadMore = () => {
+    prevLoad += 10;
+    nextLoad += 10;
+    getImgData();
+}
+
+// selection
+const imageGallery = document.getElementById('load-image-gallery');
+const loadMoreBtn = document.getElementById('load-more');
+
+// add event listener
+imageGallery.addEventListener('click', getImgData);
+loadMoreBtn.addEventListener('click', handleLoadMore);
